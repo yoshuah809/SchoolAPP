dcbad1ded704d605e263c4d31b8f0680da5b5444
unknown <yoshuah809@gmail.com>
2022-02-14 11:38:18 -0600

    created project and added school class and constructor

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    class School
+    { 
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private string _twitterAddress;
+        public string TwitterAddress
+        {
+            //Requires address starts with @ symbol
+            get { return _twitterAddress; }
+
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                }else{
+                    throw new Exception("Twitter Address musb beging with @ Symbol");
+                }
+            }
+        }
+
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-5555";
+        }
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+           Name = SchoolName;
+           PhoneNumber = SchoolPhoneNumber; 
+        }
+    }
+      
+}
