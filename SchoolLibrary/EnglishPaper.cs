3a7a476524bb6f0eb5851d73f14ed43798b68ce1
unknown <yoshuah809@gmail.com>
2022-02-14 17:00:43 -0600

    Created Interface to implement on classes

---
+++ SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    internal class EnglishPaper
+    {
+        public string  Tittle { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
+    }
+}
---
+++ SchoolLibrary/ScienceExperiment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    internal class ScienceExperiment
+    {
+        public string Hypotesys { get; set; }
+        public string Material{ get; set; }
+
+        public string Method { get; set; }
+        public string Conclusion { get; set; }
+    }
+}
